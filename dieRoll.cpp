1f415a368853e1c5842512ff491293714e0f4baa
Sarkon Kyo <ilmar.keio@gmail.com>
2022-12-09 16:08:37 +0100

    Update dieRoll.cpp

--- dieRoll.cpp
+++ dieRoll.cpp
@@ -1,7 +1,17 @@
-#include "functions.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector> 
+#include <iomanip>
+#include <cmath>
+#include <stdlib.h> //För att använda Rand
+using namespace std; 
+
+
+//default argument for 6 sided die
 
 const int SIX_SIDES= 6; 
-int rollDie();
+int rollDie(int = SIX_SIDES);
 void displayRollResult(int);
 int main(){
     int roll = 0;
@@ -14,8 +24,8 @@ return 0;
 void displayRollResult(int result){
     cout << "You just rolled a " << result << endl;
 }
-int rollDie(){
+int rollDie(int numsides){
     int roll =0; 
-    roll = rand() % SIX_SIDES +1; 
+    roll = rand() % numsides +1; 
     return roll;
 }
